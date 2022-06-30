e41e74cefefb1de51b7b515fd8ffa658007bac59
GiiGs Thanet <gigslovegod@gmail.com>
2022-06-30 17:38:46 +0700

    Component  Theme&Language

---
+++ src/components/utility/LanguageSwitcher.vue
@@ -0,0 +1,29 @@
+<template>
+    <button class="px-2 py-1 border border-yellow-400" :class="[lang ==='en'?'bg-black':'bg-gray-500']" @click="switchLang('en')">EN</button>
+    <button class="px-2 py-1 border border-yellow-400" :class="[lang ==='th'?'bg-black':'bg-gray-500']" @click="switchLang('th')">TH</button>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                lang: null
+            }
+        },
+        created() {
+            var lang = localStorage.getItem('lang')
+            if (lang === null || lang === undefined) {
+                localStorage.setItem('lang', 'en')
+                this.lang = 'en'
+            } else {
+                localStorage.setItem('lang', lang)
+                this.lang = lang
+            }
+        },
+        methods: {
+            switchLang(lang) {
+                localStorage.setItem('lang', lang)
+                this.lang = lang
+            }
+        }
+    }
+</script>
---
+++ src/components/utility/ThemeSwitcher.vue
@@ -0,0 +1,29 @@
+<template>
+    <button class="px-2 py-1 border border-yellow-400 mr-2" :class="[theme==='dark'?'bg-black':'bg-gray-500']" @click="switchTheme('dark')">Dark</button><button
+        class="px-2 py-1 border border-yellow-400" :class="[theme==='white'?'bg-black':'bg-gray-500']" @click="switchTheme('white')">White</button>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            theme:null
+        }
+    },
+    created(){
+        var theme = localStorage.getItem('theme')
+        if(theme === null || theme === undefined){
+            localStorage.setItem('theme','dark')
+            this.theme = 'dark'
+        }else{
+            localStorage.setItem('theme',theme)
+            this.theme = theme
+        }
+    },
+    methods:{
+        switchTheme(theme){
+            localStorage.setItem('theme',theme)
+            this.theme = theme
+        }
+    }
+}
+</script>
