975ff8283e4eb24545b2cff40f46c42fed4e403e
GiiGs Thanet <gigslovegod@gmail.com>
2022-07-04 17:30:02 +0700

    Set Language

--- src/components/utility/LanguageSwitcher.vue
+++ src/components/utility/LanguageSwitcher.vue
@@ -1,28 +1,20 @@
 <template>
-    <button class="px-2 py-1 border border-yellow-400" :class="[lang ==='en'?'bg-black':'bg-gray-500']" @click="switchLang('en')">EN</button>
-    <button class="px-2 py-1 border border-yellow-400" :class="[lang ==='th'?'bg-black':'bg-gray-500']" @click="switchLang('th')">TH</button>
+    <button class="px-2 py-1 border border-yellow-400" :class="[locale ==='en'?'bg-black':'bg-gray-500']" @click="switchLocale('en')">EN</button>
+    <button class="px-2 py-1 border border-yellow-400" :class="[locale ==='th'?'bg-black':'bg-gray-500']" @click="switchLocale('th')">TH</button>
 </template>
 <script>
+import { loadLocaleMessages,setI18nLanguage,i18n } from '../../i18n'
     export default {
         data() {
             return {
-                lang: null
-            }
-        },
-        created() {
-            var lang = localStorage.getItem('lang')
-            if (lang === null || lang === undefined) {
-                localStorage.setItem('lang', 'en')
-                this.lang = 'en'
-            } else {
-                localStorage.setItem('lang', lang)
-                this.lang = lang
+                locale: 'en'
             }
         },
         methods: {
-            switchLang(lang) {
-                localStorage.setItem('lang', lang)
-                this.lang = lang
+            switchLocale(locale) {
+                this.locale = locale
+                loadLocaleMessages(i18n, locale)
+                setI18nLanguage(i18n, locale)
             }
         }
     }
--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/style.css'
-
+import { i18n } from './i18n'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(i18n)
 
 app.mount('#app')
