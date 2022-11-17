09de2ac44370af6c4e2575f8a6880e22d02bfe68
GiiGs Thanet <gigslovegod@gmail.com>
2022-11-17 23:02:55 +0700

    Fix Test Rtsp

--- src/views/TestRtsp.vue
+++ src/views/TestRtsp.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
         <h1>Test Video RTSP</h1>
-        <video src="rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mp4"></video>
+        <video src=""></video>
     </div>
 </template>
